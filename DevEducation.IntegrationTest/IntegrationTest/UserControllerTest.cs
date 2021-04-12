0392d45221b62ef59394b59ffad12dafbaac1164
SimonShvets <goodweather.spb@gmail.com>
2021-04-12 19:23:57 +0300

    Solution created

---
+++ DevEducation.IntegrationTest/IntegrationTest/UserControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace IntegrationTest
+{
+    public class UserControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
