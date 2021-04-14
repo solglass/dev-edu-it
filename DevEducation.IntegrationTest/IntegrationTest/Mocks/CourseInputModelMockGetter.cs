2c10460d2c56640574a48a3cf73ab4ee18965af8
solglass <solglass@yandex.ru>
2021-04-14 20:50:27 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ DevEducation.IntegrationTest/IntegrationTest/Mocks/CourseInputModelMockGetter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using IntegrationTest.Models.InputModels;
+namespace IntegrationTest.Mocks
+{
+
+    public static class CourseInputModelMockGetter
+    {
+        public static CourseInputModel GetCourseInputModelMock(int id)
+        {
+            return id switch
+            {
+                1 => new CourseInputModel
+                {
+                    Description = "Course Test 1 mock",
+                    Duration = 8,
+                    Name = "Test 1 C#",
+                    ThemeIds=new List<int>(),
+                    MaterialIds = new List<int>()
+                },
+                2 => new CourseInputModel
+                {
+                    Description = "Course Test 2 mock",
+                    Duration = 16,
+                    Name = "Test 2 C#",
+                    ThemeIds = new List<int>(),
+                    MaterialIds = new List<int>()
+                },
+                3 => new CourseInputModel
+                {
+                    Description = "Course Test 3 mock",
+                    Duration = 32,
+                    Name = "Test 3 C#",
+                    ThemeIds = new List<int>(),
+                    MaterialIds = new List<int>()
+                },
+                4 => new CourseInputModel
+                {
+                    Description = "Course Test 4 mock",
+                    Duration = 4,
+                    Name = "Test 4 C#",
+                    ThemeIds = new List<int>(),
+                    MaterialIds = new List<int>()
+                },
+                5 => new CourseInputModel
+                {
+                    Description = "Course Test 5 mock",
+                    Duration = 2,
+                    Name = "Test 5 C#",
+                    ThemeIds = new List<int>(),
+                    MaterialIds = new List<int>()
+                },
+                6=> new CourseInputModel(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ DevEducation.IntegrationTest/IntegrationTest/Mocks/CourseOutputModelMockGetter.cs
@@ -0,0 +1,60 @@
+﻿using EducationSystem.Data.Models;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EducationSystem.Data.Tests.Mocks
+{
+    [ExcludeFromCodeCoverage]
+    public static class CourseMockGetter
+    {
+        public static CourseDto GetCourseDtoMock(int id)
+        {
+            return id switch
+            {
+                1 => new CourseDto
+                {
+                    Description = "Course Test 1 mock",
+                    Duration = 8,
+                    Name = "Test 1 C#",
+                    IsDeleted = false,
+                    Themes=new List<ThemeDto>(),
+                    Materials = new List<MaterialDto>()
+                },
+                2 => new CourseDto
+                {
+                    Description = "Course Test 2 mock",
+                    Duration = 16,
+                    Name = "Test 2 C#",
+                    Themes = new List<ThemeDto>(),
+                    Materials = new List<MaterialDto>()
+                },
+                3 => new CourseDto
+                {
+                    Description = "Course Test 3 mock",
+                    Duration = 32,
+                    Name = "Test 3 C#",
+                    Themes = new List<ThemeDto>(),
+                    Materials = new List<MaterialDto>()
+                },
+                4 => new CourseDto
+                {
+                    Description = "Course Test 4 mock",
+                    Duration = 4,
+                    Name = "Test 4 C#",
+                    Themes = new List<ThemeDto>(),
+                    Materials = new List<MaterialDto>()
+                },
+                5 => new CourseDto
+                {
+                    Description = "Course Test 5 mock",
+                    Duration = 2,
+                    Name = "Test 5 C#",
+                    Themes = new List<ThemeDto>(),
+                    Materials = new List<MaterialDto>()
+                },
+                6=> new CourseDto(),
+                _ => null,
+            };
+        }
+    }
+}
