573e29d7992245a3296c59bcfe6018a0350658cf
solglass <solglass@yandex.ru>
2021-05-13 23:22:02 +0300

    related entities for Course TestCaseSource

--- DevEducation.IntegrationTest/IntegrationTest/Tests/CourseControllerTest.cs
+++ DevEducation.IntegrationTest/IntegrationTest/Tests/CourseControllerTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using Dapper;
 using IntegrationTest.Mocks;
 using System.Data;
+using System.Collections;
 
 namespace IntegrationTest
 {
@@ -21,34 +22,37 @@ namespace IntegrationTest
             _client.SetupClient();
         }
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        public void AddCourse_ValidCourseInputModelSent_OkResponseGot_RecievedCourseModelMatchesExpected(int mockId)
+        [Test, TestCaseSource(typeof(ValidCourseWithoutThemesAndMaterialsMockIds))]
+        public void AddCourse_ValidCourseInputModelSent_OkResponseGot_RecievedCourseModelMatchesExpected(int[] mockIds)
         {
-            //Given
-            var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
+            foreach (int mockId in mockIds)
+            {
+                //Given
+                var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
 
-            var inputModel = CourseMockGetter.GetInputModel(mockId);
+                var inputModel = CourseMockGetter.GetInputModel(mockId);
 
-            _request = _client.FormPostRequest<CourseInputModel>(TestHelper.Course_Create, inputModel);
+                _request = _client.FormPostRequest<CourseInputModel>(TestHelper.Course_Create, inputModel);
 
-            //When
-            var response = _client.Execute<CourseOutputModel>(_request);
-            var actualOutputModel = response.Data;
-            Assert.IsTrue(actualOutputModel.Id != 0);
+                //When
+                var response = _client.Execute<CourseOutputModel>(_request);
+                var actualOutputModel = response.Data;
+                Assert.IsTrue(actualOutputModel.Id != 0);
 
 
-            //Then
-            expectedOutputModel.Id = actualOutputModel.Id;
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            Assert.AreEqual(expectedOutputModel, actualOutputModel);
+                //Then
+                expectedOutputModel.Id = actualOutputModel.Id;
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.AreEqual(expectedOutputModel, actualOutputModel);
+            }
+        
         }
 
 
-        [TestCase(6)]
+        [Test, TestCaseSource(typeof(EmptyCourseMockId))]
         public void AddCourse_EmptyCourseInputModelSent_ConflictResponseGot_MessageIsNotNull(int mockId)
         {
+
             //Given
             var expectedOutputModel = CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
 
@@ -65,56 +69,73 @@ namespace IntegrationTest
             Assert.AreEqual(HttpStatusCode.Conflict, response.StatusCode);
             Assert.IsNotNull(message);
         }
+        
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(5)]
-        public void GetCourse_ValidCourseIdSent_OkResponseGot_RecievedCourseModelMatchesExpected(int mockId)
+        [Test, TestCaseSource(typeof(ValidCourseWithoutThemesAndMaterialsMockIds))]
+        public void GetCourse_ValidCourseIdSent_OkResponseGot_RecievedCourseModelMatchesExpected(int[] mockIds)
         {
-            //Given
-            var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
+            foreach (int mockId in mockIds)
+            {
+                //Given
+                var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
 
-            var inputModel = (CourseInputModel)CourseMockGetter.GetInputModel(mockId).Clone();
-            _request = _client.FormPostRequest(TestHelper.Course_Create, inputModel);
-            var response = _client.Execute<CourseOutputModel>(_request);
-            var addedOutputModel = response.Data;
+                var inputModel = (CourseInputModel)CourseMockGetter.GetInputModel(mockId).Clone();
+                _request = _client.FormPostRequest(TestHelper.Course_Create, inputModel);
+                var response = _client.Execute<CourseOutputModel>(_request);
+                var addedOutputModel = response.Data;
 
-            //When 
-            _request = _client.FormGetRequest<CourseInputModel>($"{TestHelper.Course_Get}/{ addedOutputModel.Id}");
-            var responseGet = _client.Execute<CourseOutputModel>(_request);
-            var actualStatusCode = responseGet.StatusCode;
-            var actualOutputModel = responseGet.Data;
+                //When 
+                _request = _client.FormGetRequest<CourseInputModel>($"{TestHelper.Course_Get}/{ addedOutputModel.Id}");
+                var responseGet = _client.Execute<CourseOutputModel>(_request);
+                var actualStatusCode = responseGet.StatusCode;
+                var actualOutputModel = responseGet.Data;
 
-            //Then
+                //Then
 
-            Assert.AreEqual(HttpStatusCode.OK, actualStatusCode);
-            Assert.AreEqual(expectedOutputModel, actualOutputModel);
-            CollectionAssert.AreEqual(expectedOutputModel.Themes, actualOutputModel.Themes);
-            CollectionAssert.AreEqual(expectedOutputModel.Materials, actualOutputModel.Materials);
+                Assert.AreEqual(HttpStatusCode.OK, actualStatusCode);
+                Assert.AreEqual(expectedOutputModel, actualOutputModel);
+                CollectionAssert.AreEqual(expectedOutputModel.Themes, actualOutputModel.Themes);
+                CollectionAssert.AreEqual(expectedOutputModel.Materials, actualOutputModel.Materials);
+            }
         }
 
-        [TestCase(1)]
-        public void GetCourseWithThemesAndMaterials_ValidCourseIdSent_OkResponseGot_RecievedCourseModelMatchesExpected(int mockId)
+        [Test, TestCaseSource(typeof(ValidCourseWithThemesAndMaterialsMockIds))]
+        public void GetCourseWithThemesAndMaterials_ValidCourseIdSent_OkResponseGot_RecievedCourseModelMatchesExpected(int courseMockId, int[] themesMockIds, int[] materialsMockIds)
         {
             //Given
-            var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(mockId).Clone();
+            var themesMocks = new List<ThemeInputModel>();
+            var materialsMocks = new List<MaterialInputModel>();
+            var themesExpected = new List<ThemeOutputModel>();
+            var materialsExpected = new List<MaterialOutputModel>();
+            var expectedOutputModel = (CourseOutputModel)CourseOutputModelMockGetter.GetCourseOutputModelMock(courseMockId).Clone();
 
-            var inputModel = (CourseInputModel)CourseMockGetter.GetInputModel(mockId).Clone();
-            _request = _client.FormPostRequest(TestHelper.Course_Create, inputModel);
+            var inputModel = (CourseInputModel)CourseMockGetter.GetInputModel(courseMockId).Clone();
+            _request = _client.FormPostRequest<CourseInputModel>(TestHelper.Course_Create, inputModel);
             var response = _client.Execute<CourseOutputModel>(_request);
             var addedOutputModel = response.Data;
 
-            var ThemeinputModel = (ThemeInputModel)ThemeMockGetter.GetInputModel(mockId).Clone();
-            _request = _client.FormPostRequest(TestHelper.Theme_Create, inputModel);
-            var responseTheme = _client.Execute<ThemeOutputModel>(_request);
-            var addedThemeOutputModel = response.Data;
 
-            var MaterialinputModel = (MaterialInputModel)MaterialMockGetter.GetInputModel(mockId).Clone();
-            _request = _client.FormPostRequest(TestHelper.Material_Create, inputModel);
-            var responseMaterial = _client.Execute<MaterialOutputModel>(_request);
-            var addedMaterialOutputModel = response.Data;
+            foreach (int themeMockId in themesMockIds)
+            {
+                var themeInputModel = (ThemeInputModel)ThemeMockGetter.GetInputModel(themeMockId).Clone();
+                themesMocks.Add(themeInputModel);
+                _request = _client.FormPostRequest<ThemeInputModel>(TestHelper.Theme_Create, themeInputModel);
+                var responseTheme = _client.Execute<ThemeOutputModel>(_request);
+                var addedThemeOutputModel = responseTheme.Data;
+                themesExpected.Add(addedThemeOutputModel);
+            }
+            expectedOutputModel.Themes = themesExpected;
+
+            foreach (int themeMockId in themesMockIds)
+            {
+                var materialinputModel = (MaterialInputModel)MaterialMockGetter.GetInputModel(courseMockId).Clone();
+                materialsMocks.Add(materialinputModel);
+                _request = _client.FormPostRequest(TestHelper.Material_Create, materialinputModel);
+                var responseMaterial = _client.Execute<MaterialOutputModel>(_request);
+                var addedMaterialOutputModel = responseMaterial.Data;
+                materialsExpected.Add(addedMaterialOutputModel);
+            }
+            expectedOutputModel.Materials = materialsExpected;
 
             //When 
             _request = _client.FormGetRequest<CourseInputModel>($"{TestHelper.Course_Get}/{ addedOutputModel.Id}");
@@ -126,8 +147,12 @@ namespace IntegrationTest
 
             Assert.AreEqual(HttpStatusCode.OK, actualStatusCode);
             Assert.AreEqual(expectedOutputModel, actualOutputModel);
-            CollectionAssert.AreEqual(expectedOutputModel.Themes, actualOutputModel.Themes);
-            CollectionAssert.AreEqual(expectedOutputModel.Materials, actualOutputModel.Materials);
+            //TODO
+            //themesExpected => actualOutputModel.Themes
+            CollectionAssert.AreEqual(expectedOutputModel.Themes, themesExpected);
+            //TODO
+            //materialsExpected => actualOutputModel.Materials
+            CollectionAssert.AreEqual(expectedOutputModel.Materials, materialsExpected);
         }
 
         [Test]
@@ -184,7 +209,7 @@ namespace IntegrationTest
         {
             //Given
             var inputModel = (CourseInputModel)CourseMockGetter.GetInputModel(mockId).Clone();
-            
+
             //When
             _request = _client.FormPutRequest<CourseInputModel>($"{TestHelper.Course_Update}/{TestHelper.Invalid_ID}", inputModel);
             var responseGet = _client.Execute<CourseOutputModel>(_request);
@@ -289,5 +314,30 @@ namespace IntegrationTest
             BaseTest.Connection.Execute("dbo.CleanDatabase", commandType: CommandType.StoredProcedure);
         }
 
+
+
+    }
+
+    internal class ValidCourseWithThemesAndMaterialsMockIds : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { 1, new int[] { 1, 2 }, new int[] { 1, 2 } };
+        }
+    }
+    internal class ValidCourseWithoutThemesAndMaterialsMockIds : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new int[] { 1, 2, 3, 4, 5 };
+        }
+    }
+
+    internal class EmptyCourseMockId : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new int[] { 6 };
+        }
     }
 }
